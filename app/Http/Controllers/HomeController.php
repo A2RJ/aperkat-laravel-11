c8949942d0913a79dea88cabaa728bd9dc9d49fa
ARDIANSYAH PUTRA <xcz.ardiansyahputra2468@gmail.com>
2024-04-30 14:06:47 +0800

    Update add ppuf based on month

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ppuf;
-use App\Models\Submission;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
@@ -20,65 +19,47 @@ class HomeController extends Controller
 
     public function dashboard()
     {
-        $months = [
-            'januari',
-            'februari',
-            'maret',
-            'april',
-            'mei',
-            'juni',
-            'juli',
-            'agustus',
-            'september',
-            'oktober',
-            'november',
-            'desember',
-        ];
-
+        $monthsInThisYear = ['maret', 'april', 'mei', 'juni', 'juli', 'agustus', 'september', 'oktober', 'november', 'desember'];
+        $monthsInNextYear = ['januari', 'februari'];
         $ppuf_month = [];
 
-        foreach ($months as $month) {
+        foreach ($monthsInThisYear as $month) {
             $ppuf_month[strtolower($month) . ' ' . date('Y')] = 0;
         }
-        foreach ($months as $month) {
+        foreach ($monthsInNextYear as $month) {
             $ppuf_month[strtolower($month) . ' ' . (date('Y') + 1)] = 0;
         }
 
         $ppufs = Ppuf::whereIn('period', [date('Y'), date('Y') + 1])
-            ->orderByRaw("FIELD(`date`, '" . implode("','", $months) . "')")
+            ->orderByRaw("FIELD(`date`, '" . implode("','", array_merge($monthsInNextYear, $monthsInThisYear)) . "')")
             ->get();
 
         foreach ($ppufs as $ppuf) {
             $key = strtolower($ppuf->date) . ' ' . $ppuf->period;
-            $ppuf_month[$key]++;
+            if (array_key_exists($key, $ppuf_month)) {
+                $ppuf_month[$key]++;
+            }
         }
 
+        return $ppuf_month;
+
         $user = User::query()
-            ->whereHas('role', function (Builder $query) {
-                $query->whereHas('ppuf');
-            })
+            ->whereHas('role', fn (Builder $query) => $query->whereHas('ppuf'))
             ->get();
 
         $rkat = Ppuf::query()
             ->with('submissions')
             ->where('period', date('Y'))
+            ->orWhere('period', date('Y') + 1)
             ->get();
 
         $totalRkat = $rkat->count();
-        $totalRab = $rkat->pluck('budget')->sum();
-        $totalPengajuan = $rkat->sum(function ($rkat) {
-            return $rkat->submissions->count();
-        });
-        $totalRabDiajukan = $rkat->flatMap(function ($rkat) {
-            return $rkat->submissions;
-        })->pluck('budget')->sum();
-        $totalRabDisetujui = $rkat->flatMap(function ($rkat) {
-            return $rkat->submissions;
-        })->pluck('approved_budget')->sum();
+        $totalRab = $rkat->pluck('budget')->each(fn ($item) => intval($item))->sum();
+        $totalPengajuan = $rkat->sum(fn ($rkat) => $rkat->submissions->count());
+        $totalRabDiajukan = $rkat->flatMap(fn ($rkat) => $rkat->submissions)->pluck('budget')->each(fn ($item) => intval($item))->sum();
+        $totalRabDisetujui = $rkat->flatMap(fn ($rkat) => $rkat->submissions)->pluck('approved_budget')->each(fn ($item) => intval($item))->sum();
 
-        $totalRoleMengajukan = User::whereHas('role', function ($query) {
-            $query->whereHas('ppuf.submissions');
-        })->count();
+        $totalRoleMengajukan = User::whereHas('role', fn ($query) => $query->whereHas('ppuf.submissions'))->count();
 
         $output = [
             'total_rkat' => $totalRkat,
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -147,33 +147,49 @@
 <div class="row">
 
     <!-- Content Column -->
-    <div class="col-lg-6 mb-4">
+    <div class="col-lg-12 mb-4">
 
         <!-- Project Card Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
+                <h6 class="m-0 font-weight-bold text-primary">PPUF Perbulan</h6>
             </div>
             <div class="card-body">
-                <h4 class="small font-weight-bold">Server Migration <span class="float-right">20%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <h4 class="small font-weight-bold">Sales Tracking <span class="float-right">40%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <h4 class="small font-weight-bold">Customer Database <span class="float-right">60%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <h4 class="small font-weight-bold">Payout Details <span class="float-right">80%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <h4 class="small font-weight-bold">Account Setup <span class="float-right">Complete!</span></h4>
-                <div class="progress">
-                    <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Bulan</th>
+                                <th>Jumlah PPUF</th>
+                                <th>RAB PPUF</th>
+                                <th>Jumlah Pengajuan</th>
+                                <th>RAB Pengajuan</th>
+                                <th>Persentase Diajukan</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Bulan</th>
+                                <th>Jumlah PPUF</th>
+                                <th>RAB PPUF</th>
+                                <th>Jumlah Pengajuan</th>
+                                <th>RAB Pengajuan</th>
+                                <th>Persentase Diajukan</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            @foreach ($ppuf_month as $ppuf)
+                            <tr>
+                                <td>Tiger Nixon</td>
+                                <td>System Architect</td>
+                                <td>Edinburgh</td>
+                                <td>61</td>
+                                <td>2011/04/25</td>
+                                <td>$320,800</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
